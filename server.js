57ce296fa07054aaad1fda2c0069aaf8d9fa0e49
Aaren Chiou <wowchiou@gmail.com>
2020-02-03 14:58:36 +0800

    first upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000, () => {
+  console.log('Websocket server listening on : 9000');
+});
+
+const io = socketio(expressServer);
+
+io.on('connection', socket => {
+  socket.emit('welcome', { text: '歡迎來到 socket.io server!' });
+
+  socket.on('message', msg => {
+    console.log(msg);
+    io.emit('messageFromServer', { text: msg.text });
+  });
+});
